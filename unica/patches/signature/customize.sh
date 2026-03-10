fac1139390dcd128a600935153547fb3663465b0
regnermax36-glitch <regnermax36@gmail.com>
2026-03-10 17:49:52 +0100

    Update customize.sh

--- unica/patches/signature/customize.sh
+++ unica/patches/signature/customize.sh
@@ -1,12 +1,1 @@
-CERT_PREFIX="aosp_platform"
-$ROM_IS_OFFICIAL && CERT_PREFIX="platform"
 
-CERT_SIGNATURE=$(cat "$SRC_DIR/security/${CERT_PREFIX}.x509.pem" | \
-    sed '/CERTIFICATE/d' | tr -d '\n' | base64 -d | xxd -p -c 0)
-
-FTP="
-system/framework/services.jar/smali_classes2/com/android/server/pm/InstallPackageHelper.smali
-"
-for f in $FTP; do
-    sed -i "s|PUT SIGNATURE HERE|$CERT_SIGNATURE|g" "$APKTOOL_DIR/$f"
-done
